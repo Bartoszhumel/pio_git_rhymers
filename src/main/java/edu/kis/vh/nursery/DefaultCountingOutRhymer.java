24d7cc6e1d4eca491d92d4f5b109b68fadeaecc3
Wiktor Sztuk <235990@edu.p.lodz.pl>
2022-04-20 12:54:48 +0200

    5. dodanie stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,8 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public int total = -1;
-    private final int[] numbers = new int[12];
+    public static int INT = -1;
+    public static int INT1 = 12;
+    public int total = INT;
+    private int[] numbers = new int[INT1];
 
     public void countIn(int in) {
         if (!isFull())
@@ -11,22 +13,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INT;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == INT1-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return INT;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return INT;
         return numbers[total--];
     }
 
