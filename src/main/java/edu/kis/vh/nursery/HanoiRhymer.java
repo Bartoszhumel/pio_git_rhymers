e0f2ec8fd6fe0d3c0c004fcee54f0643a13027fc
Bartosz Humel <235876@edu.p.lodz.pl>
2022-05-03 12:16:06 +0200

    usuniecie wszystkich zmian

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,45 +1,19 @@
 package edu.kis.vh.nursery;
 
-/**
- *
- *
- */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    private int totalRejected = 0;
+    int totalRejected = 0;
 
-    /**
-     * @return getTotalRejected
-     */
     public int reportRejected() {
-        return getTotalRejected();
-    }
-
-    /**
-     * @param in
-     */
-    @Override
-    public void countIn(int in) {
-        if (!callCheck() && in > peekaboo())
-            setTotalRejected(getTotalRejected() + 1);
-        else
-            super.countIn(in);
-    }
-
-    /**
-     * @return totalRejected
-     */
-    public int getTotalRejected() {
         return totalRejected;
     }
 
-    /**
-     * @param totalRejected
-     * set totalRejected
-     */
-    public void setTotalRejected(int totalRejected) {
-        this.totalRejected = totalRejected;
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
     }
 }
 //wiersze 5,7,11-16
-// alt+ strzałki zmiana karty
+// alt+ strzaĹ‚ki zmiana karty
